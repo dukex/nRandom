bb9d8ba3407a66a170040be0d67e87f6d808cd72
Duke <srdukex@gmail.com>
2015-12-16 16:39:06 -0200

    update to add random images

---
+++ app.js
@@ -0,0 +1,83 @@
+//app 
+(function($) {
+	
+    var self = new function(){};
+	
+    $.extend(self, {
+
+    	Backgrounds : [
+    		'http://www.away.com.br/nvampeta/bg1.png'
+    	],
+
+        Imgs : [
+        	'http://lorempixel.com/400/200/'
+        ],
+
+        handleImages : function (lstImgs, time)
+        {
+            $.each($('img'), function(i,item) { 
+                //Skip if image is already replaced
+                if($.inArray($(item).attr('src'), lstImgs) == -1)
+                {
+					var h = $(item).height();
+					var w = $(item).width();
+					
+					//If image loaded
+					if(h > 0 && w > 0)
+					{
+						//Replace
+						$(item).css('width', w + 'px').css('height', h + 'px');
+						$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+					}
+					else
+					{
+						//Replace when loaded
+						$(item).load(function(){
+							//Prevent 'infinite' loop
+							if($.inArray($(item).attr('src'), lstImgs) == -1)
+							{
+								var h = $(item).height();
+								var w = $(item).width();
+								$(item).css('width', w + 'px').css('height', h + 'px');
+								$(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]); 
+							}
+						});
+					}
+				}
+            });
+
+            //Keep replacing
+            if (time > 0) {
+                setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+            }
+        },
+
+        handleLogo : function (bgImgs, time)
+        {
+			$backgroundImages = $(
+            	'[class*=logo], [class*=header], [id*=header], [id*=logo],' +
+            	'[class*=logo] span, [class*=header] span, [id*=header] span, [id*=logo] span,' +
+            	'[class*=logo] h1, [class*=header] h1, [id*=header] h1, [id*=logo] h1,'+
+            	'[class*=logo] a, [class*=header] a, [id*=header] a, [id*=logo] a'
+            	)
+            	.filter(function() {
+            		backgroundImg = $(this).css('background-image');
+            		return backgroundImg && backgroundImg != 'none';
+            	}
+            );
+
+            $backgroundImages.each(function(i, item) {
+            	$(item).css('background-image', 'url(http://lorempixel.com/400/200/)');
+            	$(item).css('background-position', '0 0');
+            	$(item).css('background-repeat', 'no-repeat');
+            	$(item).css('background-size', 'contain');
+            });
+        }
+    });
+
+    //Run on jQuery ready
+    $(function(){
+        self.handleImages(self.Imgs, 3000);
+     	self.handleLogo(self.Backgrounds, 3000);
+    });
+})(jQuery);
